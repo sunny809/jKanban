dda66791909c4441571634076471d8bfcc52e2a6
Sunny Deng <sunny809@gmail.com>
2019-06-06 18:58:16 +0800

    adding developer service

---
+++ src/test/java/online/tangbk/kanban/service/DeveloperServiceTest.java
@@ -0,0 +1,22 @@
+package online.tangbk.kanban.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+public class DeveloperServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
